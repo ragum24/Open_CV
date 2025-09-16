0c9473d9c1ba599e3456455d737d52fd4424ad33
ragum24 <165797568+ragum24@users.noreply.github.com>
2025-09-16 19:28:57 +1000

    Add files via upload

--- shapes_in_cv2.py
+++ shapes_in_cv2.py
@@ -1,10 +1,22 @@
 import cv2
 
+num1=629
+
+num2=497
+
 shoop=cv2.imread("moon.png")
 
 line=cv2.line(shoop,(0,0),(629,497),(233,24,255),5)
 
-cv2.imshow("moon_in_half",line)
+rect=cv2.rectangle(shoop,(50,10),(580,480),(90,20,34),5)
+
+circle=cv2.circle(shoop,(num1//2,num2//2),233,(255),5)
+
+text=cv2.putText(shoop,"DA MOOOOONNNN",(45,100),cv2.FONT_HERSHEY_TRIPLEX,2,(21, 120, 1))
+
+cv2.imshow("moon_in_half",rect)
+
+#cv2.imshow("moon",line)
 
 cv2.waitKey(0)
 
